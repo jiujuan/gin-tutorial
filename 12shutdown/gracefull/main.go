dd4ca9c2c913d1a46517c62ae4b8e42dc1c36c2e
jiujuan <jiujuanfeng@163.com>
2020-04-23 16:30:22 +0800

    shutdown

---
+++ 12shutdown/gracefull/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/person", func(c *gin.Context) {
+		time.Sleep(8 * time.Second)
+		c.String(200, "server hello")
+	})
+
+	srv := http.Server{
+		Addr:    ":5000",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("shutdown server ... ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("server shutdown: ", err)
+	}
+	log.Println("server exiting")
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gin-tutorial
 go 1.12
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.4.0
 	github.com/jinzhu/gorm v1.9.10
 	github.com/kr/pretty v0.1.0 // indirect
